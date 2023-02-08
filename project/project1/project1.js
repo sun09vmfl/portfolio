ae3a2ef30552026a3ef9222aa8c33e376f787056
sun09vmfl <colin_first@naver.com>
2023-02-08 09:38:07 +0900

    0208 오전

--- project/project1/project1.js
+++ project/project1/project1.js
@@ -1,3 +1,14 @@
-document.querySelector('#toggle-button').addEventListener('click', function () {
-  document.querySelector('.section.collapsible').classList.toggle('collapsed');
-});
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
